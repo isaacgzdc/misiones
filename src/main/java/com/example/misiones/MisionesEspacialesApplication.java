cac5ab23c9a32b0f650d5e0049d145af8af8c183
isaacgzdc <isaacgzdc@gmail.com>
2021-12-04 10:21:47 +0100

    initial project setup

---
+++ src/main/java/com/example/misiones/MisionesEspacialesApplication.java
@@ -0,0 +1,13 @@
+package com.example.misiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisionesEspacialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisionesEspacialesApplication.class, args);
+	}
+
+}
